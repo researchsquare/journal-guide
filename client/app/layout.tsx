e37305179326b8bef83167a6a83b97a787e0ea66
rohithsv <rohith.sv@mpslimited.com>
2025-09-09 12:09:02 +0000

    reverted bootstrap import

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import ReduxProvider from "@/src/providers/ReduxProvider";
 import type { ReactNode } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
