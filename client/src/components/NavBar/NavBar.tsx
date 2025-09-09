d56af5f2d929a998f662f1b3394070ac0f42ae2f
ajithkumar-MPS <ajithkumar.s@mpslimited.com>
2025-09-09 17:40:26 +0530

    nav changes

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -50,24 +50,31 @@ const NavBar = ({title}:NavBarProps) => {
         },
     ]
     return (
-        <div className='NavBar'>
-            <div>
-                {
-                    nav.map((item, index) => (
-                        <div key={index}>
-                            <div>
-                                {item.menu}
-                            </div>
-                            <div>
-                                {item.subMenu && item.subMenu.map((subItem, subIndex) => (
-                                    <div key={subIndex}>
-                                        {subItem.name}
-                                    </div>
-                                ))}
-                            </div>
+        <div className="container-fluid navBar shadow ">
+            <div className="row align-items-center">
+                {/* Left side (menu) */}
+                <div className="col-md-9">
+                <div className="d-flex justify-content-evenly p-4">
+                    {nav.map((item, index) => (
+                    <div key={index}>
+                        <div>{item.menu} &#129171;</div>
+                        <div className="hide">
+                        {item.subMenu &&
+                            item.subMenu.map((subItem, subIndex) => (
+                            <div key={subIndex}>{subItem.name}</div>
+                            ))}
                         </div>
-                    ))
-                }
+                    </div>
+                    ))}
+                </div>
+                </div>
+
+                {/* Right side (institutions & businesses) */}
+                <div className="col-md-3 text-end">
+                <div className="btn p-2">
+                    Institutions & Businesses
+                </div>
+                </div>
             </div>
         </div>
     );
