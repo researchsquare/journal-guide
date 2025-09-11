803dfe11929caea277a8e14602a2e3ea1264cd3c
ajithkumar-MPS <ajithkumar.s@mpslimited.com>
2025-09-11 10:51:39 +0530

    mobile design

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -7,7 +7,9 @@ interface NavBarProps {
 }
 
 const NavBar = ({title}:NavBarProps) => {
-     const [openIndex, setOpenIndex] = useState<number | null>(null);
+    const [openIndex, setOpenIndex] = useState<number | null>(null);
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const [selectedMenu, setSelectedMenu] = useState<number | null>(null);
 
     const nav = [
         {
@@ -156,56 +158,108 @@ const NavBar = ({title}:NavBarProps) => {
         },
     ];
 
-
     return (
-        <div className="container-fluid navBar shadow ">
-            <div className="row align-items-center">
+        <div className="container-fluid navBar shadow">
+            {/* ---------- DESKTOP NAV ---------- */}
+            <div className="row align-items-center d-none d-md-flex">
                 <div className="col-md-9">
                 <div className="d-flex justify-content-evenly p-2">
                     {nav.map((item, index) => (
-                    <div key={index} className='menuItem'>
-                        <div className={`${openIndex === index?'active':'nonActive'} mainMenu`} onClick={()=>setOpenIndex(openIndex === index? null:index)}>{item.menu} &#129171;</div>
+                    <div key={index} className="menuItem">
+                        <div
+                        className={`${openIndex === index ? 'active' : 'nonActive'} mainMenu`}
+                        onClick={() => setOpenIndex(openIndex === index ? null : index)}
+                        >
+                        {item.menu} &#129171;
+                        </div>
                         <div className={`${openIndex === index ? 'showSubMenu' : 'hideSubMenu'} p-3`}>
-                            <div className="row">
-                                {item.subMenu &&
-                                item.subMenu.map((subItem, subIndex) => (
-                                    <div key={subIndex} className="col-md-3">
-                                    <div className="submenu-title fw-bold ">{subItem.name}</div>
-                                    <div className="underline"></div>
-                                    <div className='pt-2'>
-                                        {subItem.items.map((subSubItem, subSubIndex) => (
-                                            <div key={subSubIndex} className="pb-3">
-                                                <div className="row align-items-center">
-                                                    <div className="col-auto">
-                                                    <div className="icon"></div>
-                                                    </div>
-
-                                                    <div className="col ps-0">
-                                                    <a
-                                                        href={subSubItem.link}
-                                                        className="text-decoration-none text-dark"
-                                                    >
-                                                        {subSubItem.name}
-                                                    </a>
-                                                    <div className="subDescription">{subSubItem.des}</div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
+                        <div className="row">
+                            {item.subMenu &&
+                            item.subMenu.map((subItem, subIndex) => (
+                                <div key={subIndex} className="col-md-3">
+                                <div className="submenu-title fw-bold">{subItem.name}</div>
+                                <div className="underline"></div>
+                                <div className="pt-2">
+                                    {subItem.items.map((subSubItem, subSubIndex) => (
+                                    <div key={subSubIndex} className="pb-3">
+                                        <div className="row align-items-center">
+                                        <div className="col-auto">
+                                            <div className="icon"></div>
+                                        </div>
+                                        <div className="col ps-0">
+                                            <a href={subSubItem.link} className="text-decoration-none text-dark">
+                                            {subSubItem.name}
+                                            </a>
+                                            <div className="subDescription">{subSubItem.des}</div>
+                                        </div>
+                                        </div>
                                     </div>
-                                    </div>
-                                ))}
-                            </div>
+                                    ))}
+                                </div>
+                                </div>
+                            ))}
+                        </div>
                         </div>
                     </div>
                     ))}
                 </div>
                 </div>
                 <div className="col-md-3 text-end">
-                <div className="btn p-2">
-                    Institutions & Businesses
+                <div className="btn p-2">Institutions & Businesses</div>
+                </div>
+            </div>
+
+            {/* ---------- MOBILE NAV ---------- */}
+            <div className="d-block d-md-none">
+                {/* Top Bar */}
+                <div className="d-flex justify-content-between align-items-center p-2">
+                <div className="fw-bold">{title}</div>
+                <div className="hamburger" onClick={() => setMobileOpen(!mobileOpen)}>
+                    ☰
                 </div>
                 </div>
+
+                {mobileOpen && (
+                <div className="mobileMenu">
+                    {/* Show Main Menu List */}
+                    {selectedMenu === null && (
+                    <div className="mainMenuList">
+                        {nav.map((item, index) => (
+                        <div
+                            key={index}
+                            className="mobileMainMenu"
+                            onClick={() => setSelectedMenu(index)}
+                        >
+                            {item.menu}
+                        </div>
+                        ))}
+                    </div>
+                    )}
+
+                    {/* Show Selected Submenu */}
+                    {selectedMenu !== null && (
+                    <div className="subMenuList">
+                        {/* Back Arrow */}
+                        <div className="backArrow" onClick={() => setSelectedMenu(null)}>
+                        ← Back
+                        </div>
+                        {nav[selectedMenu].subMenu.map((subItem, subIndex) => (
+                        <div key={subIndex}>
+                            <div className="fw-bold">{subItem.name}</div>
+                            {subItem.items.map((subSubItem, subSubIndex) => (
+                            <div key={subSubIndex} className="ps-3 py-2">
+                                <a href={subSubItem.link} className="text-decoration-none text-dark">
+                                {subSubItem.name}
+                                </a>
+                                <div className="subDescription">{subSubItem.des}</div>
+                            </div>
+                            ))}
+                        </div>
+                        ))}
+                    </div>
+                    )}
+                </div>
+                )}
             </div>
         </div>
     );
